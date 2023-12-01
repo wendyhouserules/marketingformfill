05c033cc5012e4e25758fc1355e8824bcbe1fca3
wendyhouserules <chris.daniel.hennessy@gmail.com>
2023-12-01 19:12:25 +0000

    Delete form-ga-client-id.js

--- form-ga-client-id.js
+++
@@ -1,8 +0,0 @@
-<script>
-(function () {
-  var value = "{{GA Client ID}}"
-  var selector = "#gaclientid"
-  var field = document.querySelector(selector)
-  if(field){ field.value = value||"(none)"; }
-})();
-</script>
